e641950632e93dbbdf3bd06009b7cc8d96e01f20
HamzaKapasi53 <113715209+HamzaKapasi53@users.noreply.github.com>
2023-03-02 09:52:38 +0530

    Create city_templist.dart

---
+++ Mobile Programming/Dart/Practical 2/city_templist.dart
@@ -0,0 +1,29 @@
+void main()
+{
+   List list = ['Mumbai',24,'Pune',20,'Nashik',18,'Nagpur',15,'Nanded',25]; 
+  
+   var max=list[1];
+   var min=list[1];
+  var mincityindex=0;
+  var maxcityindex=0;
+  
+  
+  for(int i=1;i<=list.length;i=i+2){
+    if (list[i] < min){
+      min=list[i];
+      mincityindex=i-1;
+      }
+     if (list[i] > max){
+      max=list[i];
+      maxcityindex=i-1;
+      }
+  }
+  print(min);
+  print(list[mincityindex]);
+  
+  print("${list[maxcityindex]}  ${max}");
+  print(max);
+  
+  
+}
+  
